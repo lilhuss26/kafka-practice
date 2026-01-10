17487efbcad8a4bc194e8b7b03beaf6fcc656bce
Hussam ElNemr <hassom20042019@gmail.com>
2026-01-10 20:05:29 +0200

    Intial commit

---
+++ src/main/java/org/example/Consumer.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Properties;
+import java.util.Collections;
+import java.time.Duration;
+
+public class Consumer {
+}
---
+++ src/main/java/org/example/Producer.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.common.serialization.StringSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Properties;
+import java.util.UUID;
+
+public class Producer {
+}
